1f5267282d6dcdc47b5ebebe151731910da12024
shawn lim <shawn@codigo.sg>
2017-10-04 12:04:54 +0800

    changed from [babel-preset-es2015] to newer [babel-preset-env]

--- webpack.config.js
+++ webpack.config.js
@@ -154,7 +154,7 @@ console.log("===================================================================
 				exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react', 'es2015']
+                    presets: ['react', 'env']
                 }
 			},
 
