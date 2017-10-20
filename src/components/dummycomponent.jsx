3b22dde06740e6ea09dfbd31e2f08b790be56ab3
Han 2 <han2@Timothys-iMac.local>
2017-10-20 09:52:13 +0800

    scss now working, react components are able to render from main

---
+++ src/components/dummycomponent.jsx
@@ -0,0 +1,14 @@
+import React, {PropTypes} from 'react';
+
+export default class dummcomponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>MyComponent</div>);
+  }
+}
+
+dummcomponent.propTypes = {
+};
--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,14 @@
 	}
 
 require('../scss/bundle-scss.scss') // SCSS IMPORT
-
+import App from '../App';
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
